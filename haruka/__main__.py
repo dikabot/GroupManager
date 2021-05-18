6f0bae65d7e90790896d10da4358b2a0f01baa32
dikabot <82655491+dikabot@users.noreply.github.com>
2021-05-18 14:26:17 +0000

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -20,26 +20,12 @@ from haruka.modules.helper_funcs.misc import paginate_modules
 from haruka.modules.translations.strings import tld, tld_help 
 from haruka.modules.connection import connected
 
-PM_START = """Hello {}, my name is {}!
-
-You know how hard it is sometimes to manage group so here is the solution for you
-
-I'm group manager bot and Anti-spam for RR Players🃏
-
-Activate Anti-spam protection in your BM by this command : 
-/antispam on 
-
-Made from the source code of [Aditya 🇮🇳](t.me/xditya)
-
-Special thanks to [this person](t.me/denver02) for helping me.
-
-Deploy a bot like me [now](www.github.com/xditya/GroupManager)
-
-Watch [video tutorial](https://www.youtube.com/watch?v=gXXFpTAk6Vo&feature=youtu.be) on deploying me.
-
-Click /help or Help button below to find out more about how to use me to my full potential.
-
- Add me to a group by clicking [here](http://t.me/tg_GroupManagerBot?startgroup=true).
+PM_START = """Hello {}, Nama Saya {}!
+Saya adalah bot pintar yang di buat khusus untuk mengelola grup anda
+Tambahkan saya kedalam grup sampah anda 
+Lainnya kasih gabut pc aja ownernya.
+[Owner](https://t.me/dikafake)
+[Join Sini](https://t.me/publikchatting)
 """
 
 
@@ -157,7 +143,7 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START
 
-    keyboard = [[InlineKeyboardButton(text="🇮🇳 Language", callback_data="set_lang_")]]
+    keyboard = [[InlineKeyboardButton(text="Language", callback_data="set_lang_")]]
     keyboard += [[InlineKeyboardButton(text="🛠 Reporting", callback_data="cntrl_panel_M"), 
         InlineKeyboardButton(text="❔ Help", callback_data="help_back")]]
 
